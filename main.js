b25654d3168764a37536495a85fa902131d49a29
Thaissa <thaissa.zac@gmail.com>
2024-07-28 13:13:52 -0300

    imagnes mapa celeste

---
+++ main.js
@@ -0,0 +1,43 @@
+function toggleMenu() {
+    var menuLinks = document.querySelector(".menu-links");
+    menuLinks.classList.toggle("active");
+}
+
+function rotateCard(button) {
+    const card = button.closest('.card');
+    card.classList.toggle('flip');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtém o caminho da URL atual
+    var path = window.location.pathname;
+
+    // Remove a barra inicial, se houver
+    path = path.replace(/^\/|\/$/g, '');
+
+    // Encontra o link correspondente ao caminho atual
+    var activeLink = document.querySelector('.nav-link[href="' + path + '"]');
+
+    // Adiciona a classe 'active' ao link correspondente
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+});
+
+// Ativar a classe 'active' no link clicado
+var links = document.querySelectorAll('.nav-link');
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        links.forEach(function(innerLink) {
+            innerLink.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
+var btn_BackTop = document.getElementById("btn-voltar");
+
+btn_BackTop.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+
+})
